1fce6a5cc17e5020ca6463546260a7df8cc3971a
Kevin <kevpadilla01@gmail.com>
2023-04-12 21:47:50 -0300

    Configurando base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: 'https://github.com/KevinPadi/NFT-card-component'
+    base: 'https://kevinpadi.github.io/NFT-card-component/'
 })
